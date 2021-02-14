bda0f24b2c3b19d0532244c49eb472f8aa28b237
Keach Inoue <argos.heart@gmail.com>
2021-02-15 00:18:19 +0900

    change default layer at ergo42/argos/keymap.c

--- keyboards/ergo42/keymaps/argos/keymap.c
+++ keyboards/ergo42/keymaps/argos/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,     JP_LBRC,         JP_RBRC,  KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_ESC ,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,     JP_MINS,         JP_CIRC,  KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_ENT , \
     KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,     JP_AT,           KC_QUOT,  KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  JP_BSLS, \
-    KC_LCTRL, KC_LGUI, KC_LALT, KC_INS, JP_MHEN, MO(SYMB), SFT_T(KC_SPC),   KC_RSFT,  MO(META), JP_HENK, KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT \
+    KC_LCTRL, KC_LGUI, KC_LALT, KC_INS, JP_MHEN, MO(SYMB), KC_SPC,   KC_RSFT,  MO(META), JP_HENK, KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT \
   ),
 
   /* META
