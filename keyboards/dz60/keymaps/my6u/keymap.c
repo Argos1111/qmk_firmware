0e145b51f4487ad457e824322c4d246d38360794
Keach Inoue <argos.heart@gmail.com>
2021-02-14 21:27:16 +0900

    change dz60 and gitignore

--- keyboards/dz60/keymaps/my6u/keymap.c
+++ keyboards/dz60/keymaps/my6u/keymap.c
@@ -1,29 +1,30 @@
 #include QMK_KEYBOARD_H
+#include "keymap_jp.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	LAYOUT(
 		S(KC_LBRC),KC_1,  KC_2,    KC_3,    KC_4,      KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, S(KC_MINS),KC_NO,  KC_BSPC,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, KC_BSLS,   LT(3,KC_JYEN),
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, KC_BSLS,   LT(1,KC_JYEN),
 		KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, S(KC_7), KC_ENT,
-		KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,      KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(2),   KC_NO,
-		KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,    MO(2),   KC_LEFT, KC_DOWN, KC_NO,   KC_UP,   KC_RGHT),
+		MO(2),   KC_NO,   KC_Z,    KC_X,    KC_C,      KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_NO,
+		KC_LCTL, KC_LGUI, KC_LALT, JP_MHEN, KC_SPC,    JP_HENK, KC_LEFT, KC_DOWN, KC_NO,   KC_UP,   KC_RGHT),
 
 // Layer1 function and arrow
 	LAYOUT(
-		KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,    KC_NO,KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_RBRC),S(KC_BSLS),S(KC_JYEN),
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_INS,    A(KC_GRV),
+		KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,    KC_NO,KC_DEL,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_RBRC),S(KC_BSLS),KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, C(KC_INS),S(KC_INS),A(KC_GRV),
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_8), S(KC_9), S(KC_SLSH),KC_TRNS,   KC_NO,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MHEN,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS),
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MHEN,   KC_TRNS, KC_END,  KC_PGDN,  KC_NO,   KC_PGUP, KC_HOME),
 
 // Layer2 ansi shift for jis
 	LAYOUT(
-		S(KC_EQL),S(KC_1),KC_LBRC, S(KC_3), S(KC_4),   S(KC_5), KC_EQL,  S(KC_6), S(KC_SCLN),S(KC_8), S(KC_9),  S(KC_RO),  S(KC_SCLN),KC_NO,KC_DEL,
-		KC_TRNS, S(KC_Q), S(KC_W), S(KC_E), S(KC_R),   S(KC_T), S(KC_Y), S(KC_U), S(KC_I),   S(KC_O), S(KC_P),  S(KC_RBRC),S(KC_BSLS),S(KC_JYEN),
-		KC_TRNS, S(KC_A), S(KC_S), S(KC_D), S(KC_F),   S(KC_G), S(KC_H), S(KC_J), S(KC_K),   S(KC_L), KC_QUOT,  S(KC_2),   KC_TRNS,
+		S(KC_EQL),S(KC_1),KC_LBRC, S(KC_3), S(KC_4),   S(KC_5), KC_EQL,  S(KC_6), S(KC_QUOT),S(KC_8), S(KC_9),  S(KC_RO),  S(KC_SCLN),KC_NO,KC_DEL,
+		S(KC_TAB),S(KC_Q), S(KC_W), S(KC_E), S(KC_R),   S(KC_T), S(KC_Y), S(KC_U), S(KC_I),   S(KC_O), S(KC_P),  S(KC_RBRC),S(KC_BSLS),S(KC_JYEN),
+		S(KC_ESC),S(KC_A),S(KC_S), S(KC_D), S(KC_F),   S(KC_G), S(KC_H), S(KC_J), S(KC_K),   S(KC_L), KC_QUOT,  S(KC_2),   S(KC_ENT),
 		KC_TRNS, KC_NO,   S(KC_Z), S(KC_X), S(KC_C),   S(KC_V), S(KC_B), S(KC_N), S(KC_M), S(KC_COMM),S(KC_DOT),S(KC_SLSH),KC_TRNS,   KC_NO,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_HENK,   KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS,   KC_TRNS),
+		S(KC_LCTL),S(KC_LGUI),S(KC_LALT),S(JP_MHEN),S(KC_SPC),S(JP_HENK),S(KC_LEFT),S(KC_DOWN),KC_NO,S(KC_UP),S(KC_RGHT)),
 
 // Layer3 qmk
 	LAYOUT(
@@ -41,5 +42,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_RGHT, KC_TRNS),
 
-
 };
+
